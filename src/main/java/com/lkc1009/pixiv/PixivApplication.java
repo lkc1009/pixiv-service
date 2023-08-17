7500f7e836822b2028c79e90e0bc61654e0ec03a
lkc1009 <lkc1009@users.noreply.github.com>
2023-08-17 17:33:40 +0800

    Initial commit

---
+++ src/main/java/com/lkc1009/pixiv/PixivApplication.java
@@ -0,0 +1,13 @@
+package com.lkc1009.pixiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixivApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lkc1009/pixiv/PixivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkc1009.pixiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

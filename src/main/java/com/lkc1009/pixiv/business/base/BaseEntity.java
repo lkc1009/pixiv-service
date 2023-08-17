d02097004a4fe37a18c0bc7be013eedada7f2dd3
lkc1009 <lkc1009@users.noreply.github.com>
2023-08-17 22:50:35 +0800

    feat: 添加BaseEntity

---
+++ src/main/java/com/lkc1009/pixiv/business/base/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.lkc1009.pixiv.business.base;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Accessors(fluent = true)
+public class BaseEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 创建时间
+     */
+    private Long createTime;
+
+    /**
+     * 更新时间
+     */
+    private Long updateTime;
+}

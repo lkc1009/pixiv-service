328a44fdcf73a6164e93f1ef5342ad62fc4ffdc2
lkc1009 <lkc1009@users.noreply.github.com>
2023-08-19 16:14:14 +0800

    fix: 认证方式oauth2 => http

--- src/main/java/com/lkc1009/pixiv/framework/swagger/SwaggerConfiguration.java
+++ src/main/java/com/lkc1009/pixiv/framework/swagger/SwaggerConfiguration.java
@@ -6,17 +6,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.media.StringSchema;
-import io.swagger.v3.oas.models.parameters.Parameter;
-import io.swagger.v3.oas.models.security.OAuthFlow;
-import io.swagger.v3.oas.models.security.OAuthFlows;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.*;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
-import java.util.Collections;
 
 @Configuration
 @EnableKnife4j
@@ -33,19 +28,12 @@ public class SwaggerConfiguration {
                         .license(new License()
                                 .name("Apache 2.0")))
                 .components(new Components()
-                        .addSecuritySchemes("Authorization",
+                        .addSecuritySchemes(HttpHeaders.AUTHORIZATION,
                                 new SecurityScheme()
-                                        .type(SecurityScheme.Type.OAUTH2)
-                                        .flows(new OAuthFlows()
-                                                .authorizationCode(new OAuthFlow()
-                                                        .authorizationUrl("http://localhost:8080/oauth/authorize")))
+                                        .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT"))
-                        .addParameters("Authorization",
-                                new Parameter()
-                                        .in("header")
-                                        .schema(new StringSchema())
-                                        .name("Authorization")));
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION));
     }
 
     @Bean
@@ -54,9 +42,6 @@ public class SwaggerConfiguration {
                 .group("pixiv")
                 .pathsToMatch("/pixiv/**")
                 .packagesToScan("com.lkc1009.pixiv")
-                .addOperationCustomizer((operation, handlerMethod) -> operation.security(
-                        Collections.singletonList(new SecurityRequirement().addList("Authorization"))
-                ))
                 .build();
     }
 

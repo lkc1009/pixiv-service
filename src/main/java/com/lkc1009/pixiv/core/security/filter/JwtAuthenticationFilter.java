32675696c223d14843b08a32a01ae742e1e78c41
lkc1009 <lkc1009@users.noreply.github.com>
2023-08-19 21:26:05 +0800

    delete: 删除JwtTokenProvider和TokenSecretProperties

--- src/main/java/com/lkc1009/pixiv/core/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/lkc1009/pixiv/core/security/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.lkc1009.pixiv.core.security.filter;
 
-import com.lkc1009.pixiv.core.security.util.JwtTokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,7 +16,6 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final JwtTokenProvider jwtTokenProvider;
     @Override
     protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         filterChain.doFilter(request, response);

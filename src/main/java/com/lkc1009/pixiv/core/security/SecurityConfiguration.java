83ed5fc1e0e7c31e3c6d336846cd6188eab45abf
lkc1009 <lkc1009@users.noreply.github.com>
2023-08-20 00:10:52 +0800

    delete: 删除DefaultSecurityConfiguration 合并AuthorizationClientSecurity => SecurityConfiguration

--- src/main/java/com/lkc1009/pixiv/core/security/SecurityConfiguration.java
+++ src/main/java/com/lkc1009/pixiv/core/security/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.lkc1009.pixiv.core.security;
 
-import com.lkc1009.pixiv.core.security.handler.JwtAccessDeniedHandler;
-import com.lkc1009.pixiv.core.security.handler.JwtAuthenticationEntryPoint;
 import com.lkc1009.pixiv.core.security.filter.JwtAuthenticationFilter;
 import com.lkc1009.pixiv.core.security.util.Jwks;
 import com.nimbusds.jose.jwk.JWKSet;
@@ -10,30 +8,33 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcUserInfoAuthenticationContext;
 import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcUserInfoAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -41,6 +42,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 import java.util.function.Function;
 
 @Configuration
@@ -50,55 +53,9 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
-        httpSecurity
-                .getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                .oidc(Customizer.withDefaults());
-        return httpSecurity
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(exception ->
-                        exception.authenticationEntryPoint(new JwtAuthenticationEntryPoint())
-                                .accessDeniedHandler(new JwtAccessDeniedHandler())
-                )
-                .build();
-    }
-
-    @Bean
-    public SecurityFilterChain appSecurityFilterChain(@NotNull HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .formLogin(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize ->
-                        authorize
-                                .requestMatchers("/webjars/*",
-                                        "/*/css/*",
-                                        "/*/js/*",
-                                        "/swagger*",
-                                        "/doc.html",
-                                        "/*/api-docs/*").permitAll()
-                                .anyRequest().authenticated())
-                .build();
-    }
-
-    @Bean
-    public SecurityFilterChain h2ConsoleSecurityFilterChain(@NotNull HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .securityMatcher(PathRequest.toH2Console())
-                .csrf(AbstractHttpConfigurer::disable)
-                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(Customizer.withDefaults()).disable())
-                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                        authorizationManagerRequestMatcherRegistry.anyRequest().permitAll());
-        return httpSecurity.build();
-    }
-
-    @Bean
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
-                new OAuth2AuthorizationServerConfigurer();
-        RequestMatcher endpointsMatcher = authorizationServerConfigurer
-                .getEndpointsMatcher();
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
+        RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
 
         Function<OidcUserInfoAuthenticationContext, OidcUserInfo> userInfoMapper = (context) -> {
             OidcUserInfoAuthenticationToken authentication = context.getAuthentication();
@@ -113,11 +70,9 @@ public class SecurityConfiguration {
                                 .userInfoMapper(userInfoMapper)
                         )
                 );
+
         http
                 .securityMatcher(endpointsMatcher)
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
-                )
                 .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
                 .exceptionHandling(exceptions ->
                         exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
@@ -130,13 +85,49 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        var user = User.withUsername("user")
-                .password("123456")
-                .roles("USER")
+    public SecurityFilterChain appSecurityFilterChain(@NotNull HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/webjars/*",
+                                        "/*/css/*",
+                                        "/*/js/*",
+                                        "/swagger*",
+                                        "/doc.html",
+                                        "/*/api-docs/*").permitAll()
+                                .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("pixiv")
+                .clientSecret(passwordEncoder.encode("123456"))
+                .clientAuthenticationMethods(methods -> methods
+                        .addAll(List.of(
+                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC
+                        )))
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                        .accessTokenTimeToLive(Duration.ofHours(2))
+                        .authorizationCodeTimeToLive(Duration.ofMinutes(5))
+                        .refreshTokenTimeToLive(Duration.ofDays(7))
+                        .reuseRefreshTokens(true).build())
+                .redirectUris(uris -> uris.addAll(List.of(
+                        "http://www.baidu.com"
+                )))
+                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+                .scopes(scopes -> scopes.addAll(List.of(
+                        OidcScopes.OPENID, OidcScopes.PROFILE
+                )))
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryRegisteredClientRepository(registeredClient);
     }
 
     @Bean
@@ -157,8 +148,8 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(@NotNull AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean

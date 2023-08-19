a63ede935115d8d36530ee1975dbbe793fa0aa0c
lkc1009 <lkc1009@users.noreply.github.com>
2023-08-19 14:13:41 +0800

    style: 添加注释

--- src/main/java/com/lkc1009/pixiv/business/base/BaseEntity.java
+++ src/main/java/com/lkc1009/pixiv/business/base/BaseEntity.java
@@ -2,6 +2,7 @@ package com.lkc1009.pixiv.business.base;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,20 +16,24 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @Accessors(chain = true)
+@Schema(description = "基础实体类")
 public class BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     @TableId(type = IdType.ASSIGN_ID)
+    @Schema(description = "主键")
     private Long id;
 
     /**
      * 创建时间
      */
+    @Schema(description = "创建时间")
     private Long createTime;
 
     /**
      * 更新时间
      */
+    @Schema(description = "更新时间")
     private Long updateTime;
 }
